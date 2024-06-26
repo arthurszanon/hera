fcfa7542cddbc3ac722423cca31a94bf161fc439
Lucas Eleutério Bueno <lucasbueno@frwk.com.br>
2024-06-26 13:37:44 -0300

    Tela de produtos finalizada

---
+++ src/app/components/products-list-page/products-list-page.component.ts
@@ -0,0 +1,104 @@
+import { Component } from '@angular/core';
+import {PanelMenuModule} from 'primeng/panelmenu';
+import {MenuItem} from 'primeng/api';
+import {ActivatedRoute} from '@angular/router';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {CurrencyPipe, NgForOf, NgIf} from '@angular/common';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {ProdutoService} from '../../services/produtos.service';
+
+@Component({
+  selector: 'app-products-list-page',
+  standalone: true,
+  imports: [
+    PanelMenuModule,
+    CardModule,
+    ButtonModule,
+    NgForOf,
+    CurrencyPipe,
+    NgIf,
+    ProgressSpinnerModule
+  ],
+  providers: [ProdutoService],
+  templateUrl: './products-list-page.component.html',
+  styleUrl: './products-list-page.component.css'
+})
+export class ProductsListPageComponent {
+
+  items: MenuItem[] = [];
+  categorias: any[] = [];
+
+  categoria: any;
+  produtos: any[] = [];
+
+  carrinhoPayload: any;
+  quantidadeProdutos: number = 1;
+
+  isLogged: boolean = localStorage.getItem('logged') === 'true'
+
+  loading: boolean = true;
+
+  constructor(private produtosService: ProdutoService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(){
+
+    this.produtosService.buscarCategorias().subscribe(categorias => {
+      this.categorias = categorias.map((categoria: any) => {
+        return categoria.categoria;
+      });
+
+      this.items = this.categorias.map(categoria => {
+        return {
+          label: categoria,
+          routerLink: ['/produtos', categoria]
+        }
+      });
+    });
+
+    this.route.params.subscribe(params => {
+      this.loading = true;
+      this.categoria = params['categoria'];
+      if(this.categoria){
+        this.produtosService.buscarPorCategoria(this.categoria).subscribe(produtos => {
+          this.produtos = produtos;
+          this.loading = false;
+          this.produtos.map(produto => {
+            if(produto.descricao){
+              produto.descricao = produto.descricao?.length > 32 ? produto.descricao.substring(0, 20) + '...' : produto.descricao;
+              return produto;
+            }
+            return produto;
+          });
+        });
+      }else{
+        this.produtosService.buscarProdutos().subscribe(produtos => {
+          this.produtos = produtos;
+          this.loading = false;
+          this.produtos.map(produto => {
+            if(produto.descricao){
+              produto.descricao = produto.descricao?.length > 32 ? produto.descricao.substring(0, 20) + '...' : produto.descricao;
+              return produto;
+            }
+            return produto;
+          });
+        });
+        this.categoria = 'Todos os produtos'
+      }
+    });
+
+  }
+
+  addCarrinho(product: any) {
+    this.carrinhoPayload = {
+      id: product.id,
+      nome: product.descricao,
+      descricao: product.descricao,
+      preco: product.preco,
+      quantidade: this.quantidadeProdutos,
+      imagemURL: product.imagemURL
+    }
+    this.produtosService.addToCart(this.carrinhoPayload);
+  }
+}
--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -18,4 +18,43 @@ export class ProdutoService {
   buscarPorId(id: number): Observable<any> {
     return this.http.get(this.urlBase + '/produtos/?codigo=' + id);
   }
+
+  buscarCategorias(): Observable<any> {
+    return this.http.get(this.urlBase + '/produtos/categorias');
+  }
+
+  buscarPorCategoria(categoria: string): Observable<any> {
+    return this.http.get(this.urlBase + '/produtos/categoria/' + categoria);
+  }
+
+  addToCart(product: any) {
+    const cart = localStorage.getItem('cart');
+    let cartArray = cart ? JSON.parse(cart) : [];
+    const index = cartArray.findIndex((p: any) => p.id === product.id);
+    if (index === -1) {
+      cartArray.push(product);
+    } else {
+      cartArray[index].quantidade += product.quantidade;
+    }
+    localStorage.setItem('cart', JSON.stringify(cartArray));
+  }
+
+  removeFromCart(product: any) {
+    const cart = localStorage.getItem('cart');
+    let cartArray = cart ? JSON.parse(cart) : [];
+    const index = cartArray.findIndex((p: any) => p.id === product.id);
+    if (index !== -1) {
+      cartArray.splice(index, 1);
+    }
+    localStorage.setItem('cart', JSON.stringify(cartArray));
+  }
+
+  getCart() {
+    const cart = localStorage.getItem('cart');
+    return cart ? JSON.parse(cart) : [];
+  }
+
+  clearCart() {
+    localStorage.removeItem('cart');
+  }
 }
